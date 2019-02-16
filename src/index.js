1058b43171e09ff9e3b63c7f4a42b77f2cfc8df3
Taku AMANO <taku@taaas.jp>
2019-02-16 16:48:07 +0900

    Enable to online compiling.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import domready from "domready";
+import MT from "./MT";
+
+global.React = React;
+global.MT = MT;
+
+//domready(() => {
+//  const inputEl = document.getElementById("src");
+//  const outputEl = document.getElementById("out");
+//  const src = inputEl.value;
+//
+//  function transform() {
+//    try {
+//      outputEl.innerHTML = global.Babel.transform("<div>" + inputEl.value + '</div>', {
+//        presets: ['es2015', 'react', 'stage-0']
+//      }).code;
+//
+//      const elm = document.getElementById("app");
+//      ReactDOM.render(
+//        <MT apiUrl={elm.dataset.apiUrl}>{eval(outputEl.innerHTML)}</MT>,
+//        elm
+//      );
+//    } catch (ex) {
+//      outputEl.innerHTML = 'ERROR: ' + ex.message;
+//    }
+//  }
+//  inputEl.addEventListener('keyup', transform, false);
+//  transform();
+//});
 
 domready(() => {
-  const elm = document.getElementById("app");
-  ReactDOM.render(
-    <App
-      apiUrl={elm.dataset.apiUrl}
-    />,
-    elm
-  );
+  const inputEl = document.getElementById("src");
+  const outputEl = document.getElementById("out");
+  const src = inputEl.value;
+
+  function transform() {
+    try {
+      outputEl.innerHTML = global.Babel.transform("<MT.Fragment>" + inputEl.innerHTML + '</MT.Fragment>', {
+        presets: ['es2015', 'react', 'stage-0']
+      }).code;
+
+      const elm = document.getElementById("app");
+      ReactDOM.render(
+        <MT apiUrl={elm.dataset.apiUrl}>{eval(outputEl.innerHTML)}</MT>,
+        elm
+      );
+    } catch (ex) {
+      outputEl.innerHTML = 'ERROR: ' + ex.message;
+    }
+  }
+  transform();
 });
